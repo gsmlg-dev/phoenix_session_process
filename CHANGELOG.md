0220b8b7d8aad74f7fa37a183e1177ccd7647bdb
Jonathan <3713684+gsmlg@users.noreply.github.com>
2024-09-30 10:46:03 +0800

    fix: Update session_process.ex

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,18 @@
 # Changelog
 
+## 0.3.1 (30 Sep 2024)
+
+* Fix bug.
+
+## 0.3.0 (30 Sep 2024)
+
+* Add get_session_id in process.
+
+## 0.2.0 (5 Feb 2024)
+
+* Add list_sessions and process_monitor macro.
+
 ## 0.1.0 (5 Feb 2024)
 
 * Create package.
+
--- lib/phoenix/session_process.ex
+++ lib/phoenix/session_process.ex
@@ -86,6 +86,7 @@ defmodule Phoenix.SessionProcess do
       end
 
       def get_session_id() do
+        current_pid = self()
         Registry.select(Phoenix.SessionProcess.Registry, [{{:"$1", :"$2", :_}, [{:"==", :"$2", current_pid}], [{{:"$1", :"$2"}}]}])
         |> Enum.at(0)
         |> elem(0)
@@ -111,6 +112,7 @@ defmodule Phoenix.SessionProcess do
       end
 
       def get_session_id() do
+        current_pid = self()
         Registry.select(Phoenix.SessionProcess.Registry, [{{:"$1", :"$2", :_}, [{:"==", :"$2", current_pid}], [{{:"$1", :"$2"}}]}])
         |> Enum.at(0)
         |> elem(0)
